5a2a8805abc05e0dcc79136720f4b35eb1d0ab26
Kartones <diego@kartones.net>
2019-10-23 22:54:51 +0200

    Update pillow + fix mypy issue

--- base/distance_grid.py
+++ base/distance_grid.py
@@ -11,20 +11,20 @@ Distance is represented as hexadecimal
 
 class DistanceGrid(Grid):
 
-    @property
-    def distances(self) -> Optional[Distances]:
-        return None if self._distances is None else self._distances     # mypy messes up with Any and Optional :(
-
-    @distances.setter
-    def distances(self, value: Optional[Distances]) -> None:
-        self._distances = value
-        if self._distances is not None:
-            _, self.maximum = self._distances.max
-
     def __init__(self, rows: int, columns: int) -> None:
         super().__init__(rows, columns)
         self._distances: Optional[Distances] = None
         self.maximum: int = 0
+
+    @property
+    def distances(self) -> Optional[Distances]:
+        return self._distances
+
+    @distances.setter
+    def distances(self, value: Optional[Distances]) -> None:
+        self._distances = value
+        if self._distances:
+            _, self.maximum = self._distances.max
 
     def contents_of(self, cell: Cell) -> str:
         if self.distances is not None and self.distances[cell]:
